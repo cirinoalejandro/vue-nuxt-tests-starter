dffe6107bd0371bae7a0779125c6db21ff18861b
Fack <fblind@gmail.com>
2017-02-08 10:43:22 -0300

    Add working e2e test

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const expect = require("chai").expect,
-  
+
 	config = {path: "http://localhost:3000"},
 
   //config = require("../../../express-config")(process.env),
-  
+
 
   testConfig = require("../config")(process.env);
 
@@ -13,9 +13,9 @@ const expect = require("chai").expect,
 describe("TEST", () => {
   it("should WELCOME", () => {
 
-    browser.url(`/${config.path}`);
+    browser.url(`${config.path}`);
     browser.waitForVisible("#app", 5000);
-    
+
     const result = browser.element("h1").getText();
     expect(result).to.be.eql("Welcome to Your Vue.js App");
 
